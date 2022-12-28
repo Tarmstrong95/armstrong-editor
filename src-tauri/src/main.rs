529ebb50fa93ff236df8108e973b9fb021c4ec14
Triston Armstrong <triston95strong@gmail.com>
2022-12-28 12:25:25 -0600

    get basic text editing functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
     windows_subsystem = "windows"
 )]
 
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +12,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,76 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { Box, Button, createTheme, Grid, Paper, ThemeProvider, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import { invoke } from "@tauri-apps/api";
+import { readTextFile } from '@tauri-apps/api/fs'
+import { open } from '@tauri-apps/api/dialog'
 import "./App.css";
+import Dashboard from './dashboard/Dashboard'
+import { useState, useRef, Ref } from 'react'
+import CodeEditor from '@uiw/react-textarea-code-editor';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [fileContentsRef, setFileContentsRef] = useState("")
+  const textRef = useRef<HTMLTextAreaElement>()
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const readFile = async (filePath: string) => {
+    if (!filePath) return
+    setFileContentsRef(await readTextFile(filePath))
+  }
+
+  const getFile = async () => {
+    let selectedPath: string | string[] | null = ''
+    try {
+      selectedPath = await open({
+        multiple: false,
+        title: "Open a file"
+      })
+    } catch (err) {
+      console.error(err)
+    }
+    if (typeof selectedPath === 'string') await readFile(selectedPath)
+    else {
+      alert('Cannot open this file')
+    }
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <ThemeProvider theme={darkTheme}>
+      <Paper sx={{ p: 1 }}>
+        <Grid container wrap='nowrap' gap={2} >
+          <Grid item xs={2} md={2} minWidth={150} >
+            <Paper sx={{ p: 2, height: '95vh' }} elevation={2}>
+              <Typography>You have not opened a file yet</Typography>
+              <Button onClick={getFile} size="small" variant="contained" fullWidth sx={{ mt: 1 }}>Open File</Button>
+            </Paper>
+          </Grid>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+          <Grid item xs={10} md={10}>
+            <Paper sx={{ height: '95vh', p: 2, overflow: 'auto' }} elevation={2}>
+              {!fileContentsRef.length && <Typography>Your file goes here</Typography>}
+              {!!fileContentsRef.length && <CodeEditor
+                value={fileContentsRef}
+                ref={textRef as Ref<HTMLTextAreaElement>}
+                language="js"
+                placeholder="Please enter JS code."
+                onChange={(evn) => setFileContentsRef(evn.target.value)}
+                style={{
+                  fontSize: 12,
+                  backgroundColor: 'transparent',
+                  fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                }}
+              />}
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
